e836e00f6d9842440b16d9d5ca04b10c48c5c066
Shuai Yan <syan@metropolis.io>
2025-10-12 12:05:16 -0700

    speed up quaternion

--- utils/quaternion.py
+++ utils/quaternion.py
@@ -2,29 +2,25 @@ import cupy as cp
 
 
 def quaternion_to_matrix(wxyz: cp.ndarray) -> cp.ndarray:
+    wxyz_norm = cp.linalg.norm(wxyz, axis=1, keepdims=True)
+    wxyz = wxyz / wxyz_norm
+
     w = wxyz[..., 0]
     x = wxyz[..., 1]
     y = wxyz[..., 2]
     z = wxyz[..., 3]
 
-    r00 = 1 - 2 * (y ** 2 + z ** 2)
-    r01 = 2 * (x * y - z * w)
-    r02 = 2 * (x * z + y * w)
-    r10 = 2 * (x * y + z * w)
-    r11 = 1 - 2 * (x ** 2 + z ** 2)
-    r12 = 2 * (y * z - x * w)
-    r20 = 2 * (x * z - y * w)
-    r21 = 2 * (y * z + x * w)
-    r22 = 1 - 2 * (x ** 2 + y ** 2)
+    rotation_matrix = cp.empty(wxyz.shape[:-1] + (3, 3), dtype=wxyz.dtype)
 
-    rotation_matrix = cp.stack(
-        [
-            cp.stack([r00, r01, r02], axis=-1),
-            cp.stack([r10, r11, r12], axis=-1),
-            cp.stack([r20, r21, r22], axis=-1),
-        ],
-        axis=-2,
-    )
+    rotation_matrix[..., 0, 0] = 1 - 2 * (y * y + z * z)
+    rotation_matrix[..., 0, 1] = 2 * (x * y - z * w)
+    rotation_matrix[..., 0, 2] = 2 * (x * z + y * w)
+    rotation_matrix[..., 1, 0] = 2 * (x * y + z * w)
+    rotation_matrix[..., 1, 1] = 1 - 2 * (x * x + z * z)
+    rotation_matrix[..., 1, 2] = 2 * (y * z - x * w)
+    rotation_matrix[..., 2, 0] = 2 * (x * z - y * w)
+    rotation_matrix[..., 2, 1] = 2 * (y * z + x * w)
+    rotation_matrix[..., 2, 2] = 1 - 2 * (x * x + y * y)
     return rotation_matrix
 
 
