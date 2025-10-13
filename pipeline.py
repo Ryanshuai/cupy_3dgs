381a6a5d5a4e1e7e87e09a60c7a4c612a12ce35e
Shuai Yan <syan@metropolis.io>
2025-10-13 10:29:12 -0700

    refactor pipeline

--- pipeline.py
+++ pipeline.py
@@ -1,63 +1,54 @@
 import cupy as cp
 import cv2
 
-from utils.read_ply import mu_w, sigma_w, opacity, sh_coeffs
-from utils.camera_settings import intrinsics_from_fov
+from utils.camera_settings import Camera
+from gs_core.gaussian_model import GaussianModel
 from gs_core.transforms_points import view_transform_point, calculate_projection_matrix_from_fov, project_to_ndc, \
     ndc_to_screen
 from gs_core.transforms_covariances import view_transform_covariance, calculate_intrinsic_jacobian, project_to_screen
 from gs_core.spherical_harmonecs import eval_sh
 from gs_core.rasterization import render
 
-R_view = cp.eye(3, dtype=cp.float32)
-t_view = cp.array([0, 0, -5], dtype=cp.float32)
+gm = GaussianModel.from_ply("test/test_diagonal.ply")
+gm = GaussianModel.from_ply("test/cropped_center_1of5.ply")
 
-fov_y = cp.deg2rad(45)
-screen_w = 320
-screen_h = 192
-aspect = screen_w / screen_h
-near = 0.2
-far = 10
+camera = Camera(position=[0, 0, 3], lookat=[0, 0, 0], up=[0, 1, 0],
+                fov_y=45, near=0.1, far=100, screen_w=640, screen_h=360)
 
-fx, fy, cx, cy = intrinsics_from_fov(fov_y, screen_w, screen_h)
-
-mu_c = view_transform_point(mu_w, R_view, t_view)
-
-P = calculate_projection_matrix_from_fov(fov_y, aspect, near, far)
+# gaussian center transform
+mu_c = view_transform_point(gm.mu_w, camera.R_view, camera.t_view)
+P = calculate_projection_matrix_from_fov(camera.fov_y, camera.aspect, camera.near, camera.far)
 mu_ndc = project_to_ndc(mu_c, P)
+mu_screen = ndc_to_screen(mu_ndc, camera.screen_w, camera.screen_h)
 
-margin = 1.3
-# https://github.com/graphdeco-inria/diff-gaussian-rasterization/blob/9c5c2028f6fbee2be239bc4c9421ff894fe4fbe0/cuda_rasterizer/auxiliary.h
-frustum_cull = ((mu_c[..., 2] > near)  &
-                (mu_ndc[:, 0] >= -margin) & (mu_ndc[:, 0] <= margin) &
-                (mu_ndc[:, 1] >= -margin) & (mu_ndc[:, 1] <= margin))
-
-mu_ndc = mu_ndc[frustum_cull]
-mu_c = mu_c[frustum_cull]
-sigma_w = sigma_w[frustum_cull]
-opacity = opacity[frustum_cull]
-sh_coeffs = sh_coeffs[frustum_cull]
-
-mu_screen = ndc_to_screen(mu_ndc, screen_w, screen_h)
-
-sigma_c = view_transform_covariance(sigma_w, R_view)
-J = calculate_intrinsic_jacobian(mu_c[:, 0], mu_c[:, 1], mu_c[:, 2], fx, fy)
+# gaussian covariance transform
+sigma_c = view_transform_covariance(gm.sigma_w, camera.R_view)
+J = calculate_intrinsic_jacobian(mu_c[:, 0], mu_c[:, 1], mu_c[:, 2], camera.fx, camera.fy)
 sigma_screen = project_to_screen(sigma_c, J)
 
-z_sorted_indices = cp.argsort(-mu_c[:, 2])  # Front-to-Back
-
-mu_c = mu_c[z_sorted_indices]
+# order by depth
+z_sorted_indices = cp.argsort(-mu_c[:, 2])
 mu_screen = mu_screen[z_sorted_indices]
 sigma_screen = sigma_screen[z_sorted_indices]
-opacity = opacity[z_sorted_indices]
-sh_coeffs = sh_coeffs[z_sorted_indices]
+opacity = gm.opacity[z_sorted_indices]
+sh_coeffs = gm.sh_coeffs[z_sorted_indices]
+mu_w_sorted = gm.mu_w[z_sorted_indices]
 
-colors = eval_sh(sh_coeffs, -mu_c)
+# color
+camera_center_w = -camera.t_view @ camera.R_view
+directions = mu_w_sorted - camera_center_w
+directions = directions / cp.linalg.norm(directions, axis=1, keepdims=True)
+colors = eval_sh(sh_coeffs, directions)
+colors = cp.clip(colors, 0, 1)
 
-print(f"Rendering {mu_screen.shape[0]} Gaussians...")
-image = render(mu_screen, sigma_screen, opacity, colors, screen_w, screen_h, tile_size=16)
+# render
+image = render(mu_screen, sigma_screen, opacity, colors, camera.screen_w, camera.screen_h, tile_size=16)
+
+# to cv2 image
 image = cp.clip(image, 0, 1)
 image = (image * 255).astype(cp.uint8)
 image = cp.asnumpy(image)
+image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
 cv2.imshow("image", image)
+cv2.imwrite("output.png", image)
 cv2.waitKey(0)
